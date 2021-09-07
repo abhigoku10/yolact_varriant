b8f32e64fb0fed5647384fb56801c890c80a5c1f
Madhav0499 <89854194+Madhav0499@users.noreply.github.com>
2021-09-07 19:39:09 +0530

    Add files via upload

---
+++ config/cfg_yolact.py
@@ -0,0 +1,57 @@
+batch_size = 1
+resume = None
+start_iter = 0
+num_workers = 0
+cuda  = True
+lr = None
+momentum = None
+decay = None
+gamma = None
+save_folder = 'weights/'
+config = None
+save_interval = 100
+validation_size = 5000
+validation_epoch = 2
+keep_latest = True
+keep_latest_interval = 100000
+dataset = None
+
+trained_model = 'weights/yolact_resnet50_54_800000.pth'
+top_k = 100
+cuda = True
+fast_nms = True
+display_masks = True   
+display_bboxes = True
+display_text = True
+display_scores = True
+display = False
+shuffle = False
+ap_data_file = 'results/ap_data.pkl'     
+resume = False
+max_images = -1    
+output_coco_json = False
+bbox_det_file = 'results/bbox_detections.json'
+mask_det_file =  'results/mask_detections.json'   
+config = None
+output_web_json = False
+web_det_path = 'web/dets/'
+no_bar = True
+display_lincomb = False
+benchmark = False
+no_sort = False    
+seed = None    
+mask_proto_debug = False
+no_crop = False
+image = "./input_images/street.jpg"
+images = None
+video = None
+video_multiframe = 1
+score_threshold = 0    
+dataset = None    
+detect = False    
+
+no_hash=False
+crop=True
+ 
+
+    
